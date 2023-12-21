95cf440b1167792b7e9d6ceb4a06acef21ce69a5
r-rullo <robin.rullo@logitud.fr>
2023-12-21 14:02:17 +0100

    fix qgis refresh script

--- refresh-qgis.py
+++ refresh-qgis.py
@@ -1,15 +1,37 @@
 
 import time
 import threading
+import ctypes
 
-def reload_layer():
-    while True:
-        layer=qgis.utils.iface.mapCanvas().layers()[0]
-        layer.dataProvider().forceReload()
-        layer.triggerRepaint()
-        time.sleep(2)
+class reload_layer_thread(threading.Thread):
+    def __init__(self, name):
+        threading.Thread.__init__(self)
+        self.name = name
 
-reload_thread = threading.Thread(target=reload_layer)
+    def run(self):
+        while True:
+            layer=qgis.utils.iface.mapCanvas().layers()[0]
+            layer.dataProvider().forceReload()
+            layer.triggerRepaint()
+            time.sleep(2)
+    
+    def get_id(self):
+        # returns id of the respective thread
+        if hasattr(self, '_thread_id'):
+            return self._thread_id
+        for id, thread in threading._active.items():
+            if thread is self:
+                return id
+
+    def raise_exception(self):
+        thread_id = self.get_id()
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id,
+              ctypes.py_object(SystemExit))
+        if res > 1:
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
+            print('Exception raise failure')
+
+reload_thread = reload_layer_thread("Reload Thread 1")
 reload_thread.start()
 
 
